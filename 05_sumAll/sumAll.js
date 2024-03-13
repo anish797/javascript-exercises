d28ff82e19970be7e7f6c96c16762e945ddf9872
anish797 <anishlol797@gmail.com>
2024-03-13 10:45:14 +0530

    easy tbh

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+  let sum=0;
+  if(!Number.isInteger(x) || !Number.isInteger(y)){
+    return "ERROR"
+  }
+  if(x<0 || y<0){
+    return "ERROR";
+  }
+  if(x>=y){
+    for(let i=y;i<=x;i++){
+      sum+=i;
+    }
+    return sum;
+  }else{
+    for(let i=x;i<=y;i++){
+      sum+=i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
